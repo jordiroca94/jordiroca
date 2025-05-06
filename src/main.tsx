2d53367d11166b1d56327889e411646e925618fa
Jordi Roca <jordi.roca@acaitravel.com>
2025-05-06 15:52:18 +0200

    feat: fonts setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
+import "./styles/fonts.css";
 import App from "./App.tsx";
 import "./i18n/i18n.ts";
 
