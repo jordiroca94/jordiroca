aef12744e8e1f93bede73bf5faae3a870c66d382
Jordi Roca <jordi.roca@acaitravel.com>
2025-05-08 14:02:25 +0200

    fix: Satoshi font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import "../public/fonts/fonts.css";
+import "./styles/fonts.css";
 import App from "./App.tsx";
 import "./i18n/i18n.ts";
 
